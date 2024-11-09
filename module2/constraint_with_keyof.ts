a9fa8aaa375b14659ab8c04329ef396f56c73398
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-11-09 13:58:07 +0600

    Constraint using keyof operator

---
+++ module2/constraint_with_keyof.ts
@@ -0,0 +1,38 @@
+{
+  /**
+   * ------------------------------
+   * Constraint with keyof Operator
+   * ------------------------------
+   *
+   *  constrain টা হলো type মধ্যে থাকা property গুলোকে নিয়ে union type তৈরী করে আর এটা করতে হয়ে keyof operator দিয়ে।
+   */
+
+  type Vehicle = {
+    bike: string;
+    car: string;
+    ship: string;
+  };
+
+  type Owner = "bike" | "car" | "ship"; //manually Owner type
+  type Owner2 = keyof Vehicle; // same to the Owner with keyof operator
+
+  const person1: Owner = "bike";
+  const person2: Owner2 = "ship";
+
+  const user = {
+    name: "Shahin",
+    age: 26,
+    address: "Dhaka",
+  };
+  // এখানে T এর property গুলোকে X এর মধ্যে extends করে দেয়া হয়েছে। এবং keyof দিয়ে বুঝায় ভিতরে property গুলো union type হয়ে যাবে। T এর যেকোনে একটি property X এর মধ্যে থাকতে হবে।
+  const getPropertyValue = <T, X extends keyof T>(obj: T, key: X) => {
+    return obj[key];
+  };
+
+  const name = getPropertyValue(user, "name");
+  const age = getPropertyValue(user, "age");
+
+  console.log(name, age);
+
+  //<----------------------------
+}
